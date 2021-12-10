d4dbc99342f3ed4ae4eea2a5e19e661ab81bd7fd
Jeeses313 <jesse.vilkman@hotmail.fi>
2021-12-10 18:23:02 +0200

    Modify to allow clients to add and delete players from their state

--- GameRoom/clientCommunication.js
+++ GameRoom/clientCommunication.js
@@ -1,20 +1,25 @@
+const { emit } = require("nodemon")
 
 const initializeClientCommunication = (io, playersArray) => {
-    
+
     io.on('connection', (socket) => {
         //here should be message to matchmaker about new player count and maybe message to other clients about someone joining
-        newPlayer = { id: socket.id, name: socket.handshake.query.name, x: socket.handshake.query.x, y: socket.handshake.query.y }
-        playersArray = playersArray.concat(newPlayer)
-        console.log(`user ${JSON.stringify(newPlayer)} connected`)
+        //newPlayer = { id: socket.id, name: socket.handshake.query.name, x: socket.handshake.query.x, y: socket.handshake.query.y }
+        //playersArray = playersArray.concat(newPlayer)
+        //console.log(`user ${JSON.stringify(newPlayer)} connected`)
+        console.log(`user ${socket.handshake.address} connected`)
         socket.on('disconnect', () => {
             //here should be message to matchmaker about new player count and maybe message to other clients about someone disconnecting
             disconnectedPlayer = playersArray.filter(player => player.id == socket.id)
             playersArray = playersArray.filter(player => player.id != socket.id)
-            console.log(`user ${JSON.stringify(disconnectedPlayer)} disconnected`)
+            io.emit("playerLeave", { playerID: socket.id })
+            console.log(`user ${JSON.stringify({ ...disconnectedPlayer, ip: socket.handshake.address })} disconnected`)
         })
-        socket.on("initialize", (response) => {
-            playersArray = playersArray.map(player => player.id == socket.id ? { ...player, name: response.name, y: response.y, x: response.x } : player)
-
+        socket.on("playerJoin", (response) => {
+            //playersArray = playersArray.map(player => player.id == socket.id ? { ...player, name: response.name, y: response.y, x: response.x } : player)
+            newPlayer = { id: socket.id, name: response.name, x: response.x, y: response.y }
+            playersArray = playersArray.concat(newPlayer)
+            io.emit("playerJoin", newPlayer)
         })
         socket.on("positionChange", (response) => {
             playersArray = playersArray.map(player => player.id == socket.id ? { ...player, y: response.y, x: response.x } : player)
